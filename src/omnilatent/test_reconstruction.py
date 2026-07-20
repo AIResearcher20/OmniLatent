8024d3114ca93536e6cf7c3b3fd1e0452711cea5
AIResearcher20 <sep.m1369@gmail.com>
2026-07-20 18:36:48 +0330

    Create test_reconstruction.py

---
+++ src/omnilatent/test_reconstruction.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+from config import *
+from dataloader import test_loader
+from model import OmniLatentAutoEncoder
+
+
+
+model = OmniLatentAutoEncoder(
+input_dim=INPUT_DIM,
+latent_dim=LATENT_DIM
+).to(DEVICE)
+
+
+
+model.load_state_dict(
+torch.load(
+CHECKPOINT_DIR / "best_model.pt",
+map_location=DEVICE
+)
+)
+
+model.eval()
+
+criterion = nn.MSELoss()
+
+test_loss = 0
+
+with torch.no_grad():
+
+for x in test_loader:  
+
+    x = x.to(DEVICE)  
+
+    recon, latent = model(x)  
+
+    loss = criterion(recon, x)  
+
+    test_loss += loss.item()
+
+test_loss /= len(test_loader)
+
+print("Test Loss:", test_loss)
