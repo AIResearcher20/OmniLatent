2e9a08738b2fccb4c730a910cd701939c28efcd1
AIResearcher20 <sep.m1369@gmail.com>
2026-09-19 01:11:21 +0330

    Update __init__.py

--- src/omnilatent/__init__.py
+++ src/omnilatent/__init__.py
@@ -2,6 +2,11 @@
 OmniLatent
 
 A multimodal biomedical representation learning framework.
+
+Author: Sepideh Moafi 
+Year: 2025
+License: MIT
 """
 
+__author__ = "Sepideh Moafi"
 __version__ = "0.1.0"
